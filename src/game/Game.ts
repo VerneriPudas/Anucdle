948160166c35ebc6cddfbeccd9b284e801ea1a95
VerneriPudas <verkku.pudas@gmail.com>
2025-12-02 15:49:20 +0200

    christmas mode

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -57,6 +57,7 @@ function normalize(str: string): string {
 
 // Vite special import for raw JSON
 import dailyVideosRaw from "../../data/daily_videos.json?raw";
+import christmasVideosRaw from "../../data/christmas_videos.json?raw";
 
 type Video = {
   date: string;
@@ -71,6 +72,7 @@ export class Game {
   constructor() {
     // Parse JSON into JS objects
     this.dailyVideos = JSON.parse(dailyVideosRaw);
+		this.christmasVideos = JSON.parse(christmasVideosRaw);
   }
 
   // Get today's video based on current date (YYYY-MM-DD)
@@ -92,11 +94,11 @@ export class Game {
 
 	// Pick random christmas video from christmas videos list (for christmas mode)
 	getRandomChristmasVideo(): Video | null {
-		const dates = this.dailyVideos.map((vid) => {
+		const dates = this.christmasVideos.map((vid) => {
 			return vid.date;
 		});
 		const randomDate = dates[Math.floor(Math.random() * dates.Length)];
-		return this.dailyVideos.find((vid) => vid.date === randomDate) || null;
+		return this.christmasVideos.find((vid) => vid.date === randomDate) || null;
 	}
 
   startEndlessMode(): string {
@@ -114,6 +116,15 @@ export class Game {
     const match = this.currentVideo.url.match(/[?&]v=([^&]+)/);
     return match ? match[1] : "";
   }
+
+	startChristmasMode(): string {
+    this.currentVideo = this.getRandomChristmasVideo();
+    if (!this.currentVideo) return "";
+    // Extract video ID from the URL (e.g., https://www.youtube.com/watch?v=VIDEO_ID)
+    const match = this.currentVideo.url.match(/[?&]v=([^&]+)/);
+    return match ? match[1] : "";
+  }
+
 
   checkGuess(guess: string): boolean {
     if (!this.currentVideo) return false;
